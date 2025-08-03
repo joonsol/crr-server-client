a840ecd73d4248b96ae036a17e0404dc6dcbc6a9
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-03 12:12:40 +0900

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const userRouter = require("./routes/user")
+const boardRouter = require("./routes/board")
 
 app.use(express.json())
-app.use('/users',userRouter)
+app.use('/boards',boardRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello, world");
